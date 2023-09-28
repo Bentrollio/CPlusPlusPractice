4b368224f719de598e30de945faa276c9921dcb3
Alex Baires <109702084+Bentrollio@users.noreply.github.com>
2023-09-27 20:06:10 -0400

    Add Lesson 1 initial example

---
+++ Lesson 1/Chapter1-Ex1.cpp
@@ -0,0 +1,20 @@
+/*
+ * Standard Hello World by Alex aka. Bentrollio
+ * 
+ * For testing purposes.
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string name = "Alex";
+
+    cout << "Hello again, GITHUB!" << endl;
+    cout << "I am " << name << " and I am learning how to use Git!" << endl;
+    
+    return 0;
+}
