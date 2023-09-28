e4e73ced34232e444748857b03b65d328172e3b5
Alex Baires <109702084+Bentrollio@users.noreply.github.com>
2023-09-27 20:14:04 -0400

    Added woof.cpp for funsies

---
+++ Test/woof.cpp
@@ -0,0 +1,20 @@
+/*
+ * Another standard cout toy by Alex aka. Bentrollio
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string name = "Ivan";
+    string animalSound = "woof";
+
+    cout << name << " is the best!" << endl;
+    cout << animalSound << endl;
+
+    return 0;
+
+}
